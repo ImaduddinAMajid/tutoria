abf5389e173cf3840583cf0120275351734e872b
Imaduddin A Majid <imaduddinamajid@gmail.com>
2018-10-30 11:46:01 +0300

    Use PostgreSQL
    Add postgres settings and use postgres instead of sqlite3.
    y

--- tutoria/tutoria/settings.py
+++ tutoria/tutoria/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-import django-heroku
+import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -90,6 +91,9 @@ if DEBUG:
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+DATABASES = {
+    'default': dj_database_url.config(os.environ['DATABASE_URL']),
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
